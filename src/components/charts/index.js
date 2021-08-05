f1629457ceab72d6ab3f83417590dfa067f221f9
Jonatas <jonbranaraujo18@gmail.com>
2021-08-05 14:28:50 -0300

    feat: add charts

--- src/components/charts/index.js
+++ src/components/charts/index.js
@@ -1,32 +1,40 @@
 import '../../../node_modules/react-vis/dist/style.css';
-import { XYPlot, LineSeries, XAxis, YAxis, VerticalGridLines, HorizontalGridLines } from 'react-vis';
+import { XYPlot, LineSeries, XAxis, YAxis, VerticalGridLines,VerticalBarSeries, HorizontalGridLines } from 'react-vis';
 
 const Chart = () => {
     const data = [
-        { x: 0, y: 8},
-        { x: 1, y: 5},
-        { x: 2, y: 4},
-        { x: 3, y: 9},
-        { x: 4, y: 1},
-        { x: 5, y: 7},
-        { x: 6, y: 6},
-        { x: 7, y: 3},
-        { x: 8, y: 2},
+        { x: 0, y: 1.33},
+        { x: 1, y: 1.54},
+        { x: 2, y: 1.45},
+        { x: 3, y: 1.20},
+        { x: 4, y: 1.38},
+        { x: 5, y: 1.66},
+        { x: 6, y: 1.65},
+        { x: 7, y: 1.55},
+        { x: 8, y: 1.85},
     ]
 
     return (
-        <div className={{ marginTop: '15px' }}>
-            <XYPlot height={300} width={300}>
-                <VerticalGridLines />
-                <HorizontalGridLines />
-                <XAxis />
-                <YAxis />
-                <LineSeries data={data} color="red" />
-                <LineSeries data={data} color="purple" />
-                <LineSeries data={data} color="yellow" />
-
-            </XYPlot>
-        </div>
+        <>
+            <div className={{ marginTop: '15px' }}>
+                <XYPlot height={300} width={300}>
+                    <VerticalBarSeries data={data} />
+                    <VerticalGridLines />
+                    <HorizontalGridLines />
+                    <XAxis />
+                    <YAxis />
+                </XYPlot>
+            </div>
+            <div className={{ marginTop: '15px' }}>
+                <XYPlot height={300} width={300}>
+                    <LineSeries data={data} />
+                    <VerticalGridLines />
+                    <HorizontalGridLines />
+                    <XAxis />
+                    <YAxis />
+                </XYPlot>
+            </div>
+        </>
     )
 }
 
--- src/components/main/index.js
+++ src/components/main/index.js
@@ -10,41 +10,57 @@ const Main = () => {
                 <div className="main-title">
                     <img src={hello} alt="office" />
                     <div className="main-greeting">
-                        <h1>Ola Jonatas!</h1>
+                        <h1>Olá Jonatas!</h1>
                         <p>Bem-vindo ao seu painel</p>
                     </div>
                 </div>
 
                 <div className="main-cards">
                     <div className="card">
-                        <i className="fa fa-file-text fa-2x text-lightblue"></i>
                         <div className="card-inner">
-                            <p className="text-primary-p">Numero de padidos</p>
-                            <span className="font-bold text-title">578</span>
+                            <div className="card-title">
+                                <i className="fa fa-file-text fa-2x text-lightblue"></i>
+                            </div>
+                            <div className="card-content">
+                                <p className="text-primary-p">Número de funcionários</p>        
+                                <span className="font-bold text-title">578</span>
+                            </div>
                         </div>
                     </div>
 
                     <div className="card">
-                        <i className="fa fa-money fa-2x text-red"></i>
                         <div className="card-inner">
-                            <p className="text-primary-p">Pagamentos</p>
-                            <span className="font-bold text-title">R$550,00</span>
+                            <div className="card-title">
+                                <i className="fa fa-money fa-2x text-red"></i>
+                            </div>
+                            <div className="card-content">
+                                <p className="text-primary-p">Folha de pagamento</p>
+                                <span className="font-bold text-title">R$ 1.455.000,00</span>
+                            </div>
                         </div>
                     </div>
 
                     <div className="card">
-                        <i className="fa fa-archive fa-2x text-yellow"></i>
                         <div className="card-inner">
-                            <p className="text-primary-p">Numero de produtos</p>
-                            <span className="font-bold text-title">670</span>
+                            <div className="card-title">
+                                <i className="fa fa-archive fa-2x text-yellow"></i>
+                            </div>
+                            <div className="card-content">
+                                <p className="text-primary-p">Encargos</p>
+                                <span className="font-bold text-title">R$ 218.000,00</span>
+                            </div>
                         </div>
                     </div>
 
                     <div className="card">
-                        <i className="fa fa-bars fa-2x text-green"></i>
                         <div className="card-inner">
-                            <p className="text-primary-p">Categorias</p>
-                            <span className="font-bold text-title">40</span>
+                            <div className="card-title">
+                                <i className="fa fa-bars fa-2x text-green"></i>
+                            </div>
+                            <div className="card-content">
+                                <p className="text-primary-p">Filiais</p>
+                                <span className="font-bold text-title">20</span>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -53,8 +69,8 @@ const Main = () => {
                     <div className="charts-left">
                         <div className="charts-left-title">
                             <div>
-                                <h1>Daily Reports</h1>
-                                <p>Sao Paulo, Sao Paulo, BR</p>
+                                <h1>Folha de Pagamento</h1>
+                                <p>Filial: São Paulo, SP</p>
                             </div>
                             <i className="fa fa-usd"></i>
                         </div>
@@ -72,23 +88,23 @@ const Main = () => {
                         
                         <div className="charts-right-cards">
                             <div className="card1">
-                                <h1>Lucro</h1>
-                                <p>R$ 4.000,00</p>
+                                <h1>Funcionários de férias</h1>
+                                <p>15</p>
                             </div>
 
                             <div className="card2">
-                                <h1>Pagamentos</h1>
-                                <p>R$ 250,00</p>
+                                <h1>Vale-alimentação</h1>
+                                <p>R$ 80.050,00</p>
                             </div>
 
                             <div className="card3">
-                                <h1>Custo de hospedagem</h1>
-                                <p>R$ 150,00</p>
+                                <h1>Reembolsos</h1>
+                                <p>R$ 15.000,00</p>
                             </div>
 
                             <div className="card4">
-                                <h1>Banco de dados</h1>
-                                <p>R$ 400,00</p>
+                                <h1>Banco de horas</h1>
+                                <p> 400 Horas</p>
                             </div>
                         </div>
 
